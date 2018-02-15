bd14419b32fb5e7d9e2561a06a546ebf6b63ca17
Berry  Semexan <bsemexan@gmail.com>
2018-02-15 04:21:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,7 +19,7 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  array.each {|char| char.gsub!(char[2,2], '$')}
+  array.each {|char| char.gsub!(char[2,1], '$')}
 end
 
 def find_a(array)
